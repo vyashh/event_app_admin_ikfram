bfcac6b39da884285919100fe49b3fff03f95727
unknown <vyashbhawan@gmail.com>
2020-11-10 01:16:22 +0100

    added user authentication with firebase

--- src/Signup.js
+++ src/Signup.js
@@ -1,17 +1,42 @@
-import React, { useRef } from "react";
-
-import { Form, Button, Card } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "./providers/AuthProvider";
 
 export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signUp, currentUser } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match.");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      console.log("Going to firebase");
+      await signUp(emailRef.current.value, passwordRef.current.value);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setLoading(false);
+  }
+
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
-          <Form>
+          {currentUser.email}
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
@@ -24,7 +49,7 @@ export default function Signup() {
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button type="submit" className="w-100">
+            <Button disabled={loading} type="submit" className="w-100">
               Submit
             </Button>
           </Form>
---
+++ src/providers/AuthProvider.js
@@ -0,0 +1,37 @@
+import React, { useContext, useState, useEffect } from "react";
+import { auth } from "../firebase/firebase";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+  function signUp(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signUp,
+  };
+
+  // wanneer het niet aan het laden is wordt pas de children ingeladen.
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
